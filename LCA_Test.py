5fbaad8dfdd93ae213cd52aa1fce4ccfcdd660d6
Ciaran Coady <46931188+ciarancoady98@users.noreply.github.com>
2019-09-30 10:46:54 +0100

    Added unit tests for left leaning tree

--- LCA_Test.py
+++ LCA_Test.py
@@ -4,6 +4,7 @@ import LCA
 class TestfindLCA(unittest.TestCase):
 
     def test_findLCA(self):
+        ## Test 1 ##
         #   Testing a simple balanced binary tree
         #
         #               1
@@ -23,6 +24,36 @@ class TestfindLCA(unittest.TestCase):
         self.assertEqual(LCA.findLCA(root, 4, 5), 2, "Should be 2")
         self.assertEqual(LCA.findLCA(root, 4, 6), 1, "Should be 1")
         self.assertEqual(LCA.findLCA(root, 3, 4), 1, "Should be 1")
+        self.assertEqual(LCA.findLCA(root, 2, 4), 2, "Should be 2")
+
+        ## Test 2 ##
+        #   Testing a left leaning tree
+        #
+        #                                               1
+        #                                           /
+        #                                       2
+        #                                   /
+        #                               3
+        #                           /
+        #                       4
+        #                   /
+        #               5
+        #           /
+        #       6
+        #   /
+        # 7
+        #
+        root = LCA.Node(1) 
+        root.left = LCA.Node(2) 
+        root.left.left = LCA.Node(3) 
+        root.left.left.left = LCA.Node(4) 
+        root.left.left.left.left = LCA.Node(5) 
+        root.left.left.left.left.left = LCA.Node(6) 
+        root.left.left.left.left.left.left = LCA.Node(7) 
+
+        self.assertEqual(LCA.findLCA(root, 4, 5), 4, "Should be 4")
+        self.assertEqual(LCA.findLCA(root, 4, 6), 4, "Should be 4")
+        self.assertEqual(LCA.findLCA(root, 3, 4), 3, "Should be 3")
         self.assertEqual(LCA.findLCA(root, 2, 4), 2, "Should be 2")
 
 if __name__ == '__main__':
