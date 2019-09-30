9782c9e235db7ea019bd6ebe384f027274e8f19b
Ciaran Coady <46931188+ciarancoady98@users.noreply.github.com>
2019-09-30 10:26:56 +0100

    Added some basic tests, these test check if find lca will work for a basic balanced binary tree

--- LCA_Test.py
+++ LCA_Test.py
@@ -1,25 +1,23 @@
 import unittest
+import LCA
 
+class TestfindLCA(unittest.TestCase):
 
-# Driver program to test above function 
-# Let's create the Binary Tree shown in above diagram 
-root = Node(1) 
-root.left = Node(2) 
-root.right = Node(3) 
-root.left.left = Node(4) 
-root.left.right = Node(5) 
-root.right.left = Node(6) 
-root.right.right = Node(7) 
+    def test_findLCA(self):
+        # Driver program to test above function 
+        # Let's create the Binary Tree shown in above diagram 
+        root = LCA.Node(1) 
+        root.left = LCA.Node(2) 
+        root.right = LCA.Node(3) 
+        root.left.left = LCA.Node(4) 
+        root.left.right = LCA.Node(5) 
+        root.right.left = LCA.Node(6) 
+        root.right.right = LCA.Node(7) 
 
-print("LCA(4, 5) = %d" %(findLCA(root, 4, 5,)))
-print("LCA(4, 6) = %d" %(findLCA(root, 4, 6)))
-print("LCA(3, 4) = %d" %(findLCA(root,3,4)))
-print("LCA(2, 4) = %d" %(findLCA(root,2, 4)))
-
-class TestSum(unittest.TestCase):
-
-    def test_sum(self):
-        self.assertEqual(sum([1, 2, 3]), 6, "Should be 6")
+        self.assertEqual(LCA.findLCA(root, 4, 5), 2, "Should be 2")
+        self.assertEqual(LCA.findLCA(root, 4, 6), 1, "Should be 1")
+        self.assertEqual(LCA.findLCA(root, 3, 4), 1, "Should be 1")
+        self.assertEqual(LCA.findLCA(root, 2, 4), 2, "Should be 2")
 
 if __name__ == '__main__':
     unittest.main()
