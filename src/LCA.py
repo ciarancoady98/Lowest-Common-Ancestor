d0231f46170867ded88396735dd522d7e5a1ac28
ciaran <ccoady@tcd.ie>
2019-10-13 22:11:11 +0100

    adding cases to recusive findLCA function

--- src/LCA.py
+++ src/LCA.py
@@ -11,29 +11,41 @@ class Node:
 		self.visited = False
 
 def findLCA(root, key1, key2):
-
+	
 	# If the node is null return
 	if(root is None):
+		print("root is none")
 		return -1
 
 	# If the node has been visited return as we have found a cycle
 	if(root.visited is True):
+		print(root.key, key1, key2, "cycle has occured")
 		return -1
 
 	root.visited = True
 	left_subtree = findLCA(root.left, key1, key2)
 	right_subtree = findLCA(root.right, key1, key2)
+
+	if(root.key is key1 or root.key is key2):
+		if(left_subtree is not -1):
+			print(root.key, key1, key2, "root.key matches key 1 or 2 and other key in left subtree")
+			return root.key
+		elif(right_subtree is not -1):
+			print(root.key, key1, key2, "root.key matches key 1 or 2 and other key in right subtree")
+			return root.key
+		else:
+			return root.key
 	
 	if(left_subtree is not -1 and right_subtree is not -1):
+		print(root.key, key1, key2, "key in both subtrees")
 		return root.key
 
-	if((root.key is key1 or root.key is key2) and left_subtree is not -1):
+	if(left_subtree is not -1):
+		print(root.key, key1, key2, "key in left subtree")
 		return left_subtree
 	
-	if((root.key is key1 or root.key is key2) and right_subtree is not -1):
+	if(right_subtree is not -1):
+		print(root.key, key1, key2, "key in right subtree")
 		return right_subtree
-	
-	if(root.key is key1 or root.key is key2):
-		return root.key
 
 	return -1
