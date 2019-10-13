62403951f2b080dd3005696b75b9622b94071793
Ciaran Coady <46931188+ciarancoady98@users.noreply.github.com>
2019-10-13 18:19:23 +0100

    Fleshing out recursive solution

--- src/LCA.py
+++ src/LCA.py
@@ -8,54 +8,25 @@ class Node:
 		self.key = key 
 		self.left = None
 		self.right = None
+		self.visited = False
 
-# Finds the path from root node to given root of the tree. 
-# Stores the path in a list path[], returns true if path 
-# exists otherwise false 
-def findPath( root, path, k): 
+def findLCA(root, key1, key2):
 
-	# Baes Case 
-	if root is None: 
-		return False
-
-	# Store this node is path vector. The node will be 
-	# removed if not in path from root to k 
-	path.append(root.key) 
-
-	# See if the k is same as root's key 
-	if root.key == k : 
-		return True
-
-	# Check if k is found in left or right sub-tree 
-	if ((root.left != None and findPath(root.left, path, k)) or
-			(root.right!= None and findPath(root.right, path, k))): 
-		return True
-
-	# If not present in subtree rooted with root, remove 
-	# root from path and return False 
-	
-	path.pop() 
-	return False
-
-# Returns LCA if node n1 , n2 are present in the given 
-# binary tre otherwise return -1 
-def findLCA(root, n1, n2): 
-
-	# To store paths to n1 and n2 fromthe root 
-	path1 = [] 
-	path2 = [] 
-
-	# Find paths from root to n1 and root to n2. 
-	# If either n1 or n2 is not present , return -1 
-	if (not findPath(root, path1, n1) or not findPath(root, path2, n2)): 
+	if(root == None):
 		return -1
 
-	# Compare the paths to get the first different value 
-	i = 0
-	while(i < len(path1) and i < len(path2)): 
-		if path1[i] != path2[i]: 
-			break
-		i += 1
-	return path1[i-1] 
+	if(root.key == key1 or root.key == key2):
+		return root.key
 
-# This code is contributed by Nikhil Kumar Singh(nickzuck_007) 
+	
+
+	return -1
+
+def findLCARecursive(root, key):
+	if(root.key == key):
+		return True
+	if(root.left != None):
+		return findLCARecursive(root.left, key)
+	if(root.right != None):
+		return findLCARecursive(root.right, key)
+	return False
