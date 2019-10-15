70a9d4515ee2ee0d323c30ab1972a577f153ccd8
ciaran <ccoady@tcd.ie>
2019-10-15 22:02:51 +0100

    Extended graph representation to use nodes for ease of testing

--- src/LCA.py
+++ src/LCA.py
@@ -3,6 +3,10 @@
 # traverses vertices reachable from s. 
 from collections import defaultdict 
 
+class Node:
+	def __init__(self, key):
+		self.key = key
+
 # This class represents a directed graph 
 # using adjacency list representation 
 class Graph: 
@@ -20,7 +24,7 @@ class Graph:
 
 	# Function to add an edge to graph 
 	def addEdge(self,u,v): 
-		self.graph[u].append(v)
+		self.graph[u.key].append(v)
 
 	# Function to print the graph
 	def printGraph(self):
@@ -42,65 +46,71 @@ def LCARecursive(graph, stack, visited, current, key1, key2, paths1, paths2):
 			print("Return path for key 2")
 			paths2.append(list(stack))
 		print ("start of dfs recursive : stack : ",stack," current : ",current," noOfVertices : ",graph.noOfVertices)
-		for edge in graph.graph[current]:
-			print("edge:",current,edge)
-			LCARecursive(graph, stack, visited, edge, key1, key2, paths1, paths2)
+		for node in graph.graph[current]:
+			print("edge:",current,node.key)
+			LCARecursive(graph, stack, visited, node.key, key1, key2, paths1, paths2)
 		stack.pop()
 	return -1
 	
 
-def findLCA(graph, start, key1, key2):
+def findLCA(graph, start, node1, node2):
 	if(graph is not None):
-		stack = []
-		paths1 = []
-		paths2 = []
-		LCARecursive(graph, stack, None, start, key1, key2, paths1, paths2)
-		print("paths1 : ",paths1)
-		print("paths2 : ",paths2)
-		greatestCommonAncestor = -1
-		depth = -1
-		for path1 in paths1 :
-			for path2 in paths2 :
-				len1 = len(path1)
-				len2 = len(path2)
-				length = 0
-				if(len1 >= len2):
-					length = len1
-				else:
-					length = len2
-				i = 0
-				currentCommonAncestor = -1
-				while i < length :
-					path1Node = -1
-					path2Node = -1
-					if(i<len1):
-						path1Node = path1[i]
 
-					if(i<len2):
-						path2Node = path2[i]
+		if(graph.noOfVertices > 1):
+			stack = []
+			paths1 = []
+			paths2 = []
+			LCARecursive(graph, stack, None, start, node1, node2, paths1, paths2)
+			print("paths1 : ",paths1)
+			print("paths2 : ",paths2)
+			greatestCommonAncestor = -1
+			depth = -1
+			for path1 in paths1 :
+				for path2 in paths2 :
+					len1 = len(path1)
+					len2 = len(path2)
+					length = 0
+					if(len1 >= len2):
+						length = len1
+					else:
+						length = len2
+					i = 0
+					currentCommonAncestor = -1
+					while i < length :
+						path1Node = -1
+						path2Node = -1
+						if(i<len1):
+							path1Node = path1[i]
+
+						if(i<len2):
+							path2Node = path2[i]
 						
-					if(path1Node == path2Node):
-						currentCommonAncestor = path1Node
-						print("currentCommonAncestor: ", currentCommonAncestor)
-					elif(i > depth):
-						greatestCommonAncestor = currentCommonAncestor
-						depth = i
-						print("greatestCommonAncestor : ", greatestCommonAncestor)
-						break
-					i += 1
-	print("The greatest common ancestor is : ", greatestCommonAncestor)
-	return greatestCommonAncestor
+						if(path1Node == path2Node):
+							currentCommonAncestor = path1Node
+							print("currentCommonAncestor: ", currentCommonAncestor)
+						elif(i > depth):
+							greatestCommonAncestor = currentCommonAncestor
+							depth = i
+							print("greatestCommonAncestor : ", greatestCommonAncestor)
+							break
+						i += 1
+			print("The greatest common ancestor is : ", greatestCommonAncestor)
+			return greatestCommonAncestor
+		else:
+			print("this graph has less than 1 vertex")
+	else:
+		return -1
 
 def main():
 	graph = Graph(8)
-	graph.addEdge(1,2)
-	graph.addEdge(1,3)
-	graph.addEdge(2,4)
-	graph.addEdge(2,5)
-	graph.addEdge(3,6)
-	graph.addEdge(3,7)
-	graph.addEdge(4,8)
-	graph.addEdge(5,8)
+	graph.addEdge(Node(1),Node(2))
+	graph.addEdge(Node(1),Node(3))
+	graph.addEdge(Node(2),Node(4))
+	graph.addEdge(Node(2),Node(5))
+	graph.addEdge(Node(3),Node(6))
+	graph.addEdge(Node(3),Node(7))
+	graph.addEdge(Node(4),Node(8))
+	graph.addEdge(Node(5),Node(8))
 	graph.printGraph()
 	findLCA(graph,1,4,8)
 
--- tests/LCA_Test.py
+++ tests/LCA_Test.py
@@ -12,19 +12,13 @@ class TestfindLCA(unittest.TestCase):
         self.assertEqual(LCA.findLCA(None, 1, -10, 6), -1, "Should be -1")
 
     def test_empty_graph(self):
-        graph = LCA.Graph(0)
+        graph = LCA.Graph(1)
+        graph.addEdge(LCA.Node(1), LCA.Node(1))
         #  Testing with a single leaf node and keys not in the tree
-        self.assertEqual(LCA.findLCA(graph, 1, 1, 1), 1, "Should be 1")
+        self.assertEqual(LCA.findLCA(graph, 1, 1, 1), -1, "Should be 1")
         self.assertEqual(LCA.findLCA(graph, 1, 1, 5), -1, "Should be -1")
         self.assertEqual(LCA.findLCA(graph, 1, -10, 6), -1, "Should be -1")
-    '''
-    def test_node_with_cycle(self):
-        # Testing a node that has its self as its child
-        root = LCA.Node(1)
-        root.left = root
-        root.right = root
-        self.assertEqual(LCA.findLCA(root, 1, 1), 1, "Should be 1")
-    '''
+
     def test_balanced_tree(self):
         #   Testing a simple balanced binary tree
         #
@@ -35,12 +29,12 @@ class TestfindLCA(unittest.TestCase):
         #   4       5       6       7
         #
         graph = LCA.Graph(7)
-        graph.addEdge(1,2)
-        graph.addEdge(1,3)
-        graph.addEdge(2,4)
-        graph.addEdge(2,5)
-        graph.addEdge(3,6)
-        graph.addEdge(3,7)
+        graph.addEdge(LCA.Node(1),LCA.Node(2))
+        graph.addEdge(LCA.Node(1),LCA.Node(3))
+        graph.addEdge(LCA.Node(2),LCA.Node(4))
+        graph.addEdge(LCA.Node(2),LCA.Node(5))
+        graph.addEdge(LCA.Node(3),LCA.Node(6))
+        graph.addEdge(LCA.Node(3),LCA.Node(7))
 
         self.assertEqual(LCA.findLCA(graph, 1, 4, 5), 2, "Should be 2")
         self.assertEqual(LCA.findLCA(graph, 1, 4, 6), 1, "Should be 1")
@@ -69,12 +63,12 @@ class TestfindLCA(unittest.TestCase):
         #
 
         graph = LCA.Graph(7)
-        graph.addEdge(1,2)
-        graph.addEdge(2,3)
-        graph.addEdge(3,4)
-        graph.addEdge(4,5)
-        graph.addEdge(5,6)
-        graph.addEdge(6,7)
+        graph.addEdge(LCA.Node(1),LCA.Node(2))
+        graph.addEdge(LCA.Node(2),LCA.Node(3))
+        graph.addEdge(LCA.Node(3),LCA.Node(4))
+        graph.addEdge(LCA.Node(4),LCA.Node(5))
+        graph.addEdge(LCA.Node(5),LCA.Node(6))
+        graph.addEdge(LCA.Node(6),LCA.Node(7))
 
         self.assertEqual(LCA.findLCA(graph, 1, 4, 5), 4, "Should be 4")
         self.assertEqual(LCA.findLCA(graph, 1, 4, 6), 4, "Should be 4")
@@ -94,16 +88,16 @@ class TestfindLCA(unittest.TestCase):
         #       8               9
         #
         graph = LCA.Graph(9)
-        graph.addEdge(1,2)
-        graph.addEdge(1,3)
-        graph.addEdge(2,4)
-        graph.addEdge(2,5)
-        graph.addEdge(3,6)
-        graph.addEdge(3,7)
-        graph.addEdge(4,8)
-        graph.addEdge(5,8)
-        graph.addEdge(6,9)
-        graph.addEdge(7,9)
+        graph.addEdge(LCA.Node(1),LCA.Node(2))
+        graph.addEdge(LCA.Node(1),LCA.Node(3))
+        graph.addEdge(LCA.Node(2),LCA.Node(4))
+        graph.addEdge(LCA.Node(2),LCA.Node(5))
+        graph.addEdge(LCA.Node(3),LCA.Node(6))
+        graph.addEdge(LCA.Node(3),LCA.Node(7))
+        graph.addEdge(LCA.Node(4),LCA.Node(8))
+        graph.addEdge(LCA.Node(5),LCA.Node(8))
+        graph.addEdge(LCA.Node(6),LCA.Node(9))
+        graph.addEdge(LCA.Node(7),LCA.Node(9))
 
         self.assertEqual(LCA.findLCA(graph, 1, 4, 5), 2, "Should be 2")
         self.assertEqual(LCA.findLCA(graph, 1, 4, 6), 1, "Should be 1")
