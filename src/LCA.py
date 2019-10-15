c15eea8b6493a0e428f8639ca82b9027497f3c30
Ciaran Coady <46931188+ciarancoady98@users.noreply.github.com>
2019-10-15 08:11:28 +0100

    Added comments to algorithm and some extra unit tests, which uncovered a flaw in the search algorithm

--- src/LCA.py
+++ src/LCA.py
@@ -1,7 +1,6 @@
-# Python Program for Lowest Common Ancestor in a Binary Tree 
-# O(n) solution to find LCS of two given values n1 and n2 
+# Python Program for Lowest Common Ancestor in a Directed Acyclic Graph
 
-# A binary tree node 
+# A Node in the graph 
 class Node: 
 	# Constructor to create a new binary node 
 	def __init__(self, key): 
@@ -11,7 +10,7 @@ class Node:
 		self.visited = False
 
 def findLCA(root, key1, key2):
-
+	# Check if the initial root node is null, if so don't recurse
 	if(root is not None):
 		if(root.left is None and root.right is None and (key1 is not key2)):
 			return -1
@@ -27,10 +26,15 @@ def findLCARecursive(root, key1, key2, found1, found2):
 	if(root.visited is True):
 		return -1
 
-	root.visited = True
+	# Otherwise visit this node and search its left and right subtrees
+	#root.visited = True
 	left_subtree = findLCARecursive(root.left, key1, key2, found1, found2)
 	right_subtree = findLCARecursive(root.right, key1, key2, found1, found2)
+	# After we've made the recursive call we are no longer checking for cycles
+	# so leave the node the way we found it 
+	#root.visited = False
 
+	# If we find a key, set its boolean and return the key
 	if(root.key is key1):
 		found1[0] = True
 		return root.key
@@ -38,7 +42,8 @@ def findLCARecursive(root, key1, key2, found1, found2):
 	if(root.key is key2):
 		found2[0] = True
 		return root.key
-		
+
+	# If we have found both keys do a case analysis to return the correct value	
 	if(found1[0] is True and found2[0] is True):
 		if(left_subtree is not -1 and right_subtree is not -1):
 			return root.key
@@ -47,10 +52,13 @@ def findLCARecursive(root, key1, key2, found1, found2):
 		else:
 			return right_subtree
 
+	# If execution has fallen through to here it means the keys lie in the same
+	# subtree, return the LCA
 	if(left_subtree is not -1):
 		return left_subtree
 	
 	if(right_subtree is not -1):
 		return right_subtree
 	
+	# No keys found return failure
 	return -1
--- tests/LCA_Test.py
+++ tests/LCA_Test.py
@@ -10,21 +10,18 @@ class TestfindLCA(unittest.TestCase):
         self.assertEqual(testNode.key,None, "Should be null")
 
     def test_null_tree(self):
-        ## Test 1 ##
         #  Testing a null input
         self.assertEqual(LCA.findLCA(None, 0, 0), -1, "Should be -1")
         self.assertEqual(LCA.findLCA(None, 1, 5), -1, "Should be -1")
         self.assertEqual(LCA.findLCA(None, -10, 6), -1, "Should be -1")
 
     def test_leaf_node(self):
-        ## Test 2 ##
         #  Testing with a single leaf node and keys not in the tree
         self.assertEqual(LCA.findLCA(LCA.Node(1), 1, 1), 1, "Should be 1")
         self.assertEqual(LCA.findLCA(LCA.Node(1), 1, 5), -1, "Should be -1")
         self.assertEqual(LCA.findLCA(LCA.Node(1), -10, 6), -1, "Should be -1")
 
     def test_node_with_cycle(self):
-        ## Test 3 ##
         # Testing a node that has its self as its child
         root = LCA.Node(1)
         root.left = root
@@ -32,7 +29,6 @@ class TestfindLCA(unittest.TestCase):
         self.assertEqual(LCA.findLCA(root, 1, 1), 1, "Should be 1")
 
     def test_balanced_tree(self):
-        ## Test 3 ##
         #   Testing a simple balanced binary tree
         #
         #               1
@@ -50,66 +46,11 @@ class TestfindLCA(unittest.TestCase):
         root.right.right = LCA.Node(7) 
 
         self.assertEqual(LCA.findLCA(root, 4, 5), 2, "Should be 2")
-
-    def test_balanced_tree1(self):
-        ## Test 3 ##
-        #   Testing a simple balanced binary tree
-        #
-        #               1
-        #       |               |
-        #       2               3
-        #   |       |       |       |
-        #   4       5       6       7
-        #
-        root = LCA.Node(1) 
-        root.left = LCA.Node(2) 
-        root.right = LCA.Node(3) 
-        root.left.left = LCA.Node(4) 
-        root.left.right = LCA.Node(5) 
-        root.right.left = LCA.Node(6) 
-        root.right.right = LCA.Node(7) 
-
         self.assertEqual(LCA.findLCA(root, 4, 6), 1, "Should be 1")
-
-    def test_balanced_tree2(self):
-        ## Test 3 ##
-        #   Testing a simple balanced binary tree
-        #
-        #               1
-        #       |               |
-        #       2               3
-        #   |       |       |       |
-        #   4       5       6       7
-        #
-        root = LCA.Node(1) 
-        root.left = LCA.Node(2) 
-        root.right = LCA.Node(3) 
-        root.left.left = LCA.Node(4) 
-        root.left.right = LCA.Node(5) 
-        root.right.left = LCA.Node(6) 
-        root.right.right = LCA.Node(7) 
-
         self.assertEqual(LCA.findLCA(root, 3, 4), 1, "Should be 1")
-
-    def test_balanced_tree3(self):
-        ## Test 3 ##
-        #   Testing a simple balanced binary tree
-        #
-        #               1
-        #       |               |
-        #       2               3
-        #   |       |       |       |
-        #   4       5       6       7
-        #
-        root = LCA.Node(1) 
-        root.left = LCA.Node(2) 
-        root.right = LCA.Node(3) 
-        root.left.left = LCA.Node(4) 
-        root.left.right = LCA.Node(5) 
-        root.right.left = LCA.Node(6) 
-        root.right.right = LCA.Node(7) 
-
         self.assertEqual(LCA.findLCA(root, 2, 4), 2, "Should be 2")
+
+        
         
     def test_left_leaning_tree(self):
          ## Test 4 ##
@@ -139,122 +80,10 @@ class TestfindLCA(unittest.TestCase):
         root.left.left.left.left.left.left = LCA.Node(7) 
 
         self.assertEqual(LCA.findLCA(root, 4, 5), 4, "Should be 4")
-
-    def test_left_leaning_tree1(self):
-         ## Test 4 ##
-        #   Testing a left leaning tree
-        #
-        #                                               1
-        #                                           /
-        #                                       2
-        #                                   /
-        #                               3
-        #                           /
-        #                       4
-        #                   /
-        #               5
-        #           /
-        #       6
-        #   /
-        # 7
-        #
-
-        root = LCA.Node(1) 
-        root.left = LCA.Node(2) 
-        root.left.left = LCA.Node(3) 
-        root.left.left.left = LCA.Node(4) 
-        root.left.left.left.left = LCA.Node(5) 
-        root.left.left.left.left.left = LCA.Node(6) 
-        root.left.left.left.left.left.left = LCA.Node(7) 
-
         self.assertEqual(LCA.findLCA(root, 4, 6), 4, "Should be 4")
-
-    def test_left_leaning_tree2(self):
-         ## Test 4 ##
-        #   Testing a left leaning tree
-        #
-        #                                               1
-        #                                           /
-        #                                       2
-        #                                   /
-        #                               3
-        #                           /
-        #                       4
-        #                   /
-        #               5
-        #           /
-        #       6
-        #   /
-        # 7
-        #
-
-        root = LCA.Node(1) 
-        root.left = LCA.Node(2) 
-        root.left.left = LCA.Node(3) 
-        root.left.left.left = LCA.Node(4) 
-        root.left.left.left.left = LCA.Node(5) 
-        root.left.left.left.left.left = LCA.Node(6) 
-        root.left.left.left.left.left.left = LCA.Node(7) 
-
         self.assertEqual(LCA.findLCA(root, 3, 4), 3, "Should be 3")
-
-    def test_left_leaning_tree3(self):
-         ## Test 4 ##
-        #   Testing a left leaning tree
-        #
-        #                                               1
-        #                                           /
-        #                                       2
-        #                                   /
-        #                               3
-        #                           /
-        #                       4
-        #                   /
-        #               5
-        #           /
-        #       6
-        #   /
-        # 7
-        #
-
-        root = LCA.Node(1) 
-        root.left = LCA.Node(2) 
-        root.left.left = LCA.Node(3) 
-        root.left.left.left = LCA.Node(4) 
-        root.left.left.left.left = LCA.Node(5) 
-        root.left.left.left.left.left = LCA.Node(6) 
-        root.left.left.left.left.left.left = LCA.Node(7) 
-
         self.assertEqual(LCA.findLCA(root, 2, 4), 2, "Should be 2")
-
-    def test_right_leaning_tree4(self):
-        ## Test 5 ##
-        #   Testing a right leaning tree
-        #
-        #   1
-        #       \
-        #           2
-        #               \
-        #                   3
-        #                       \
-        #                           4
-        #                               \
-        #                                   5
-        #                                       \
-        #                                           6
-        #                                               \
-        #                                                   7
-        #
         
-        root = LCA.Node(1) 
-        root.right = LCA.Node(2) 
-        root.right.right = LCA.Node(3) 
-        root.right.right.right = LCA.Node(4) 
-        root.right.right.right.right = LCA.Node(5) 
-        root.right.right.right.right.right = LCA.Node(6) 
-        root.right.right.right.right.right.right = LCA.Node(7) 
-
-        self.assertEqual(LCA.findLCA(root, 4, 5), 4, "Should be 4")
 
     def test_right_leaning_tree(self):
         ## Test 5 ##
@@ -283,39 +112,9 @@ class TestfindLCA(unittest.TestCase):
         root.right.right.right.right.right = LCA.Node(6) 
         root.right.right.right.right.right.right = LCA.Node(7) 
 
+        self.assertEqual(LCA.findLCA(root, 4, 5), 4, "Should be 4")
         self.assertEqual(LCA.findLCA(root, 4, 6), 4, "Should be 4")
-
-    def test_right_leaning_tree1(self):
-        ## Test 5 ##
-        #   Testing a right leaning tree
-        #
-        #   1
-        #       \
-        #           2
-        #               \
-        #                   3
-        #                       \
-        #                           4
-        #                               \
-        #                                   5
-        #                                       \
-        #                                           6
-        #                                               \
-        #                                                   7
-        #
-        
-        root = LCA.Node(1) 
-        root.right = LCA.Node(2) 
-        root.right.right = LCA.Node(3) 
-        root.right.right.right = LCA.Node(4) 
-        root.right.right.right.right = LCA.Node(5) 
-        root.right.right.right.right.right = LCA.Node(6) 
-        root.right.right.right.right.right.right = LCA.Node(7) 
-
         self.assertEqual(LCA.findLCA(root, 3, 4), 3, "Should be 3")
-
-            
-
 
     def test_balanced_tree_common_parent(self):
         ## Test 3 ##
@@ -344,14 +143,14 @@ class TestfindLCA(unittest.TestCase):
         root.right.right.left = sharedNode2
 
         self.assertEqual(LCA.findLCA(root, 4, 5), 2, "Should be 2")
-        #self.assertEqual(LCA.findLCA(root, 4, 6), 1, "Should be 1")
-        #self.assertEqual(LCA.findLCA(root, 3, 4), 1, "Should be 1")
-        #self.assertEqual(LCA.findLCA(root, 2, 4), 2, "Should be 2")
-        #self.assertEqual(LCA.findLCA(root, 4, 8), 4, "Should be 4")
-        #self.assertEqual(LCA.findLCA(root, 8, 4), 4, "Should be 4")
-        #self.assertEqual(LCA.findLCA(root, 8, 5), 5, "Should be 5")
-        #self.assertEqual(LCA.findLCA(root, 6, 9), 6, "Should be 6")
-        #self.assertEqual(LCA.findLCA(root, 7, 9), 7, "Should be 7")
+        self.assertEqual(LCA.findLCA(root, 4, 6), 1, "Should be 1")
+        self.assertEqual(LCA.findLCA(root, 3, 4), 1, "Should be 1")
+        self.assertEqual(LCA.findLCA(root, 2, 4), 2, "Should be 2")
+        self.assertEqual(LCA.findLCA(root, 4, 8), 4, "Should be 4")
+        self.assertEqual(LCA.findLCA(root, 8, 4), 4, "Should be 4")
+        self.assertEqual(LCA.findLCA(root, 8, 5), 5, "Should be 5")
+        self.assertEqual(LCA.findLCA(root, 6, 9), 6, "Should be 6")
+        self.assertEqual(LCA.findLCA(root, 7, 9), 7, "Should be 7")
 
 
 if __name__ == '__main__':
