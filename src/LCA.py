4032e3e09b5b493a1f65393d875c4e8284f31c98
ciaran <ccoady@tcd.ie>
2019-10-15 10:40:17 +0100

    Decided to use a DFS solution and have broken things

--- src/LCA.py
+++ src/LCA.py
@@ -17,35 +17,49 @@ class Graph:
 	def addEdge(self,u,v): 
 		self.graph[u].append(v) 
 
-	# Function to print a BFS of graph 
-	def BFS(self, s): 
+# Function to print a BFS of graph 
+def DFS(graph, start, key): 
 
-		# Mark all the vertices as not visited 
-		visited = [False] * (len(self.graph)) 
+	# Mark all the vertices as not visited 
+	visited = []
+	for(vertex in graph.graph):
+		visited[vertex] = False
 
-		# Create a queue for BFS 
-		queue = [] 
+	# Create a stack for DFS 
+	stack = [] 
 
-		# Mark the source node as 
+	# Mark the source node as 
+	# visited and enqueue it 
+	stack.append(start) 
+	visited[start] = True
+
+	while stack: 
+
+		# Dequeue a vertex from 
+		# queue and print it 
+		s = queue.pop(0) 
+		print (s, " ") 
+
+		# Get all adjacent vertices of the 
+		# dequeued vertex s. If a adjacent 
+		# has not been visited, then mark it 
 		# visited and enqueue it 
-		queue.append(s) 
-		visited[s] = True
+		for i in self.graph[s]: 
+			if visited[i] == False: 
+				queue.append(i) 
+				visited[i] = True
 
-		while queue: 
-
-			# Dequeue a vertex from 
-			# queue and print it 
-			s = queue.pop(0) 
-			print (s, " ") 
-
-			# Get all adjacent vertices of the 
-			# dequeued vertex s. If a adjacent 
-			# has not been visited, then mark it 
-			# visited and enqueue it 
-			for i in self.graph[s]: 
-				if visited[i] == False: 
-					queue.append(i) 
-					visited[i] = True
+def DFSRecursvie(graph, stack, visited, current, key):
+	# Mark the current node as visited and print it 
+    visited[current] = true; 
+    cout << v << " "; 
+  
+    // Recur for all the vertices adjacent to this vertex 
+    list<int>::iterator i; 
+    for(i = adj[v].begin(); i != adj[v].end(); ++i) 
+        if(!visited[*i]) 
+            DFSUtil(*i, visited); 
+	return -1
 
 def findLCA(graph, key1, key2):
 	return -1
