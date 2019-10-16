9673a1147c68958ce17df56de0bb5fe6851cd514
Ciaran Coady <46931188+ciarancoady98@users.noreply.github.com>
2019-10-16 11:31:05 +0100

    Merged my own LCA for a binary tree to master, now using two different LCA implementations to see fi my test code is comprehansive

--- tests/LCA_Test.py
+++ tests/LCA_Test.py
@@ -14,6 +14,10 @@ class TestfindBinaryTreeBinaryTree(unittest.TestCase):
         self.assertEqual(BinaryTree.findLCA(None, 0, 0), -1, "Should be -1")
         self.assertEqual(BinaryTree.findLCA(None, 1, 5), -1, "Should be -1")
         self.assertEqual(BinaryTree.findLCA(None, -10, 6), -1, "Should be -1")
+        
+        self.assertEqual(BinaryTree.findLCA2(None, 0, 0), -1, "Should be -1")
+        self.assertEqual(BinaryTree.findLCA2(None, 1, 5), -1, "Should be -1")
+        self.assertEqual(BinaryTree.findLCA2(None, -10, 6), -1, "Should be -1")
 
     def test_leaf_node(self):
         #  Testing with a single leaf node and keys not in the tree
@@ -21,12 +25,19 @@ class TestfindBinaryTreeBinaryTree(unittest.TestCase):
         self.assertEqual(BinaryTree.findLCA(BinaryTree.Node(1), 1, 5), -1, "Should be -1")
         self.assertEqual(BinaryTree.findLCA(BinaryTree.Node(1), -10, 6), -1, "Should be -1")
 
+        self.assertEqual(BinaryTree.findLCA2(BinaryTree.Node(1), 1, 1), 1, "Should be 1")
+        self.assertEqual(BinaryTree.findLCA2(BinaryTree.Node(1), 1, 5), -1, "Should be -1")
+        self.assertEqual(BinaryTree.findLCA2(BinaryTree.Node(1), -10, 6), -1, "Should be -1")
+
+        
+
     def test_node_with_cycle(self):
         # Testing a node that has its self as its child
         root = BinaryTree.Node(1)
         root.left = root
         root.right = root
         self.assertEqual(BinaryTree.findLCA(root, 1, 1), 1, "Should be 1")
+        self.assertEqual(BinaryTree.findLCA2(root, 1, 1), 1, "Should be 1")
 
     def test_balanced_tree(self):
         #   Testing a simple balanced binary tree
@@ -49,6 +60,11 @@ class TestfindBinaryTreeBinaryTree(unittest.TestCase):
         self.assertEqual(BinaryTree.findLCA(root, 4, 6), 1, "Should be 1")
         self.assertEqual(BinaryTree.findLCA(root, 3, 4), 1, "Should be 1")
         self.assertEqual(BinaryTree.findLCA(root, 2, 4), 2, "Should be 2")
+
+        self.assertEqual(BinaryTree.findLCA2(root, 4, 5), 2, "Should be 2")
+        self.assertEqual(BinaryTree.findLCA2(root, 4, 6), 1, "Should be 1")
+        self.assertEqual(BinaryTree.findLCA2(root, 3, 4), 1, "Should be 1")
+        self.assertEqual(BinaryTree.findLCA2(root, 2, 4), 2, "Should be 2")
 
         
         
@@ -83,6 +99,11 @@ class TestfindBinaryTreeBinaryTree(unittest.TestCase):
         self.assertEqual(BinaryTree.findLCA(root, 4, 6), 4, "Should be 4")
         self.assertEqual(BinaryTree.findLCA(root, 3, 4), 3, "Should be 3")
         self.assertEqual(BinaryTree.findLCA(root, 2, 4), 2, "Should be 2")
+
+        self.assertEqual(BinaryTree.findLCA2(root, 4, 5), 4, "Should be 4")
+        self.assertEqual(BinaryTree.findLCA2(root, 4, 6), 4, "Should be 4")
+        self.assertEqual(BinaryTree.findLCA2(root, 3, 4), 3, "Should be 3")
+        self.assertEqual(BinaryTree.findLCA2(root, 2, 4), 2, "Should be 2")
         
 
     def test_right_leaning_tree(self):
@@ -115,6 +136,11 @@ class TestfindBinaryTreeBinaryTree(unittest.TestCase):
         self.assertEqual(BinaryTree.findLCA(root, 4, 5), 4, "Should be 4")
         self.assertEqual(BinaryTree.findLCA(root, 4, 6), 4, "Should be 4")
         self.assertEqual(BinaryTree.findLCA(root, 3, 4), 3, "Should be 3")
+
+        self.assertEqual(BinaryTree.findLCA2(root, 4, 5), 4, "Should be 4")
+        self.assertEqual(BinaryTree.findLCA2(root, 4, 6), 4, "Should be 4")
+        self.assertEqual(BinaryTree.findLCA2(root, 3, 4), 3, "Should be 3")
+
 
     def test_balanced_tree_common_parent(self):
         ## Test 3 ##
@@ -151,6 +177,16 @@ class TestfindBinaryTreeBinaryTree(unittest.TestCase):
         self.assertEqual(BinaryTree.findLCA(root, 8, 5), 5, "Should be 5")
         self.assertEqual(BinaryTree.findLCA(root, 6, 9), 6, "Should be 6")
         self.assertEqual(BinaryTree.findLCA(root, 7, 9), 7, "Should be 7")
+
+        self.assertEqual(BinaryTree.findLCA2(root, 4, 5), 2, "Should be 2")
+        self.assertEqual(BinaryTree.findLCA2(root, 4, 6), 1, "Should be 1")
+        self.assertEqual(BinaryTree.findLCA2(root, 3, 4), 1, "Should be 1")
+        self.assertEqual(BinaryTree.findLCA2(root, 2, 4), 2, "Should be 2")
+        self.assertEqual(BinaryTree.findLCA2(root, 4, 8), 4, "Should be 4")
+        self.assertEqual(BinaryTree.findLCA2(root, 8, 4), 4, "Should be 4")
+        self.assertEqual(BinaryTree.findLCA2(root, 8, 5), 5, "Should be 5")
+        self.assertEqual(BinaryTree.findLCA2(root, 6, 9), 6, "Should be 6")
+        self.assertEqual(BinaryTree.findLCA2(root, 7, 9), 7, "Should be 7")
 
 
 if __name__ == '__main__':
